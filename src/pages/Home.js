6e2925152defd861441a58aefe902391ca6a10e2
Ash Gandawa <17383007+ashgandawa@users.noreply.github.com>
2022-08-18 23:16:44 +0200

    First Commit. Mjolo Web App v1

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import Nav from '../components/Nav'
+
+const Home = () => {
+
+    const authToken = true;
+    const handleClick = () => {
+        console.log('clicked');
+    }
+
+    return (
+        <div>
+            <Nav />
+
+            <div className="home">
+                <h1>Swipe Right</h1>
+                <button className="primary-button" onClick={handleClick}>
+                    {authToken ? 'Signout' : 'Create Account'}
+                </button>
+
+            </div>
+        </div>
+    );
+}
+
+export default Home;
